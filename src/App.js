9ec262f0be8435cebc178dc456c0ad70b09bfaa1
shahrookhansahil <shahroo.bscsf19@iba-suk.edu.pk>
2023-03-15 00:28:10 +0500

    deleted some unused images

--- src/App.js
+++ src/App.js
@@ -1,18 +1,98 @@
-import logo from './logo.svg';
 import './App.css';
+import Header from './Components/Header';
 import Elements from './Components/Elements';
+import { useState, useEffect } from 'react';
+
 function App() {
+
+  const [data ,setData] = useState([]);
+  useEffect(()=>{
+    const fetchData = () =>{
+      fetch('https://restcountries.com/v2/all?fields=name,region,area') //Fetching data from given Api
+      .then(response =>response.json())
+      .then(json => setData(json))
+      .catch(e=>{
+        console.log("Error", e)
+      })
+    }
+    fetchData();
+  },[])
+
   return (
     <div className="App">
-      <header className="App-header">
-       <h2>REIZ TECH HOMEWORK ASSIGNMENT</h2>
-      </header>
-      <button>Sign In</button>
-      <button>Sign Up</button>
-      <button id='srch'>Search</button>
-      <Elements/>
+      <Header/>
+      <button>Sort by Area</button>
+      <button>Sort by Region</button>
+      <button id='srch'>Sort</button>
+      <div className='main'>
+        {
+          data
+          .sort((a,b) => a.name > b.name?1:-1)
+          .map((element, index) =>{
+            return(
+              <Elements 
+                key={index}
+                name = {element.name}
+                region = {element.region}
+                area = {element.area}
+                /> 
+            )
+          })
+        }
+        
+      </div>
     </div>
   );
 }
-
 export default App;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// import './App.css';
+// import Header from './Components/Header';
+// import countruy_data from './Components/Country_Data.json';
+// import Elements from './Components/Elements';
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <Header/>
+//       <button>Sort by Area</button>
+//       <button>Sort by Region</button>
+//       <button id='srch'>Sort</button>
+      
+//       <div className='main'>
+//         {
+          
+//           countruy_data
+//           .sort((a,b) => a.name > b.name?1:-1)
+//           .map((element, index) =>{
+//             return(
+//               <Elements 
+//                 key={index}
+//                 name = {element.name}
+//                 region = {element.region}
+//                 area = {element.area}
+//                 /> 
+//             )
+//           })
+//         }
+        
+//       </div>
+//     </div>
+//   );
+// }
+
+// export default App;
--- src/Components/Elements.jsx
+++ src/Components/Elements.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
 import './Element.css';
-function Elements() {
+const Elements = (props) => {
   return (
     <div  id='elmnt'>
-        <div className='elDiv' id='d1'>Div 1</div>
-        <div className='elDiv' id='d2'>Div 2</div>
-        <div className='elDiv' id='d3'>Div 3</div>
+        <div className='elDiv' id='d1'><b>{props.name}</b></div>
+        <div className='elDiv' id='d2'><i>{props.region}</i></div>
+        <div className='elDiv' id='d3'>{props.area}</div>
     </div>
   )
 }
